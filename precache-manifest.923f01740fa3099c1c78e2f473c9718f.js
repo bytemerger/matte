26d25bb3005ae58aae094dc96604964de7ac4259
francis <onyishif@gmail.com>
2020-04-04 01:16:15 +0100

    Updates

---
+++ precache-manifest.923f01740fa3099c1c78e2f473c9718f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90aad821a8028dffe64545ee74903ce6",
+    "url": "/proalgor/index.html"
+  },
+  {
+    "revision": "5d3fbcbf813f11f1599c",
+    "url": "/proalgor/static/css/main.a8594ea9.chunk.css"
+  },
+  {
+    "revision": "8128d9a76e0726990330",
+    "url": "/proalgor/static/js/2.8d22152a.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/proalgor/static/js/2.8d22152a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3fbcbf813f11f1599c",
+    "url": "/proalgor/static/js/main.fa0cfc2c.chunk.js"
+  },
+  {
+    "revision": "871f8bc084aa21ef8d1e",
+    "url": "/proalgor/static/js/runtime-main.62cdd667.js"
+  },
+  {
+    "revision": "c8e3c14f0581c2154be59cd911cc4652",
+    "url": "/proalgor/static/media/Express Yourself.c8e3c14f.svg"
+  },
+  {
+    "revision": "c3df914c0381c67085943f5cfdcf936d",
+    "url": "/proalgor/static/media/Identity BackG.c3df914c.png"
+  },
+  {
+    "revision": "49e0a6bd5598acd977f3acd95ba74dcd",
+    "url": "/proalgor/static/media/Identity.49e0a6bd.png"
+  },
+  {
+    "revision": "24fdb11aec188e65c9060c03a256ee43",
+    "url": "/proalgor/static/media/Logo design SVG.24fdb11a.svg"
+  },
+  {
+    "revision": "3b583ea41e3e390cc7741a78b308eb75",
+    "url": "/proalgor/static/media/Matte Logo.3b583ea4.png"
+  },
+  {
+    "revision": "216b6f8995b1a0bdb284b289949a7572",
+    "url": "/proalgor/static/media/Story SVG.216b6f89.svg"
+  },
+  {
+    "revision": "d70b84a4bdde43f3b515e532afdb3691",
+    "url": "/proalgor/static/media/UI SVG.d70b84a4.svg"
+  },
+  {
+    "revision": "2e50ab8426cbb0bf731e8f50c503780a",
+    "url": "/proalgor/static/media/UiUx.2e50ab84.png"
+  },
+  {
+    "revision": "ae49f4952ceec0c9abf1a169c73c2eb2",
+    "url": "/proalgor/static/media/Web Development SVG.ae49f495.svg"
+  },
+  {
+    "revision": "713c3d7b0146147fbeaf2b293f583a6f",
+    "url": "/proalgor/static/media/blue boy background.713c3d7b.svg"
+  },
+  {
+    "revision": "362cf825389cdaa76f518fbaae41ae43",
+    "url": "/proalgor/static/media/boy edit.362cf825.svg"
+  },
+  {
+    "revision": "1098f94d5c8cd84a9fcb9af20002a247",
+    "url": "/proalgor/static/media/pawel-czerwinski-KWpm9MmEo2s-unsplash.1098f94d.jpg"
+  },
+  {
+    "revision": "3ef26cfb5ec924f4be393305a3935e61",
+    "url": "/proalgor/static/media/subscribe SVG.3ef26cfb.svg"
+  },
+  {
+    "revision": "210473d08adb5522456290c05671832c",
+    "url": "/proalgor/static/media/web.210473d0.png"
+  }
+]);
